6b66f19861e6e10f18629c79adece1280c5abe4f
Farmwell <g.ferowich@gmail.com>
2020-05-23 07:46:12 -0500

    prepare to read from quandl

--- pynance.py
+++ pynance.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
-
+import quandl
 style.use('ggplot')
 
 start = dt.datetime(2010, 1, 1)
 end = dt.datetime(2019, 12, 31)
 
+# symbol =
 df = web.DataReader('TSLA', 'yahoo', start, end)
-print(df.head)
+print(df.head(5))

852f05f0195b9e7657e07bc9367aea55e69d840a
Farmwell <g.ferowich@gmail.com>
2020-05-23 09:27:37 -0500

    constructed first graph

--- pynance.py
+++ pynance.py
@@ -11,9 +11,12 @@ style.use('ggplot')
 start = dt.datetime(2010, 1, 1)
 end = dt.datetime(2019, 12, 31)
 
-# symbol =
-# df = web.DataReader('TSLA', 'quandl', start, end,
-#                     api_key == 'KptHVXnYi7RdK5YUNGkx')
-df = quandl.get("BITFINEX/BTCUSD")
-print(df)
-df.to_csv('btc.csv')
+# # symbol =
+# # df = web.DataReader('TSLA', 'quandl', start, end,
+# #                     api_key == 'KptHVXnYi7RdK5YUNGkx')
+# df = quandl.get("BITFINEX/BTCUSD")
+# print(df)
+# df.to_csv('btc.csv')
+df = pd.read_csv('datasets/btc.csv', parse_dates=True, index_col=0)
+# print(df.head())
+df.plot()
